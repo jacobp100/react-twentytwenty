c29dba4f4f0fe9c615dacfce0f6e416b8dcc4e09
Jacob Parker <jacobparker1992@gmail.com>
2016-05-08 15:01:04 +0100

    Turn off scrolling interaction on demo (as you can't actually scroll there)

--- dist/index.js
+++ dist/index.js
@@ -70,7 +70,11 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	(0, _reactDom.render)(_react2.default.createElement(
 	  _src2.default,
-	  { verticalAlign: 'bottom' },
+	  {
+	    verticalAlign: 'bottom',
+	    minDistanceToBeginInteraction: 0,
+	    maxAngleToBeginInteraction: Infinity
+	  },
 	  _react2.default.createElement('img', { alt: 'cat-1', src: 'http://exmoorpet.com/wp-content/uploads/2012/08/cat.png', width: '200' }),
 	  _react2.default.createElement('img', { alt: 'cat-2', src: 'http://www.cats.org.uk/uploads/images/pages/photo_latest14.jpg', width: '300' }),
 	  _react2.default.createElement('div', { className: 'slider' })
